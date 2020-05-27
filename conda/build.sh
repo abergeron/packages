09b0f900cae10183245a52c8b6469e99766bb4b3
Arnaud Bergeron <abergeron@gmail.com>
2020-05-27 13:43:26 -0400

    Disable cuda 10.0

--- conda/build.sh
+++ conda/build.sh
@@ -11,8 +11,8 @@ echo "### BUILD CPU"
 ../scripts/run.sh conda_cpu ./conda/build_cpu.sh
 echo "### BUILD CUDA 9.2"
 ../scripts/run.sh conda_cuda92 ./conda/build_cuda.sh
-echo "### BUILD CUDA 10.0"
-../scripts/run.sh conda_cuda100 ./conda/build_cuda.sh
+#echo "### BUILD CUDA 10.0"
+#../scripts/run.sh conda_cuda100 ./conda/build_cuda.sh
 echo "### BUILD CUDA 10.2"
 ../scripts/run.sh conda_cuda102 ./conda/build_cuda.sh
 
