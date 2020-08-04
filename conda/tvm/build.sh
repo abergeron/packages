ff6085798962bb78dfd7b817c473f8a9b201e919
Arnaud Bergeron <abergeron@gmail.com>
2020-08-04 11:18:44 -0400

    TOPI was merged into the main package/library

--- conda/tvm/build.sh
+++ conda/tvm/build.sh
@@ -6,7 +6,3 @@ set -u
 cd python
 python setup.py install --single-version-externally-managed --record=/tmp/record.txt
 cd ..
-
-cd topi/python
-python setup.py install --single-version-externally-managed --record=/tmp/record.txt
-cd ../..
